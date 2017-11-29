c2f03a4e3f8ac1534ce38921c4ccb6e6f5c88f16
Oscar Sanchez <oscar.sanchez@orbis.com.pe>
2017-11-29 16:56:15 -0500

    feat(AWS) testing cloud watch log

--- app/index.php
+++ app/index.php
@@ -2,14 +2,20 @@
 require 'vendor/autoload.php';
 use Aws\Sdk;
 
+$credentials = new Aws\Credentials\Credentials('AKIAIVFQMFY4Z3HI2XRA', 'fCY060Q75oZqwYnny7DVP5q+fMt7rHHuhf2SggLN');
+
 $config = [
-    'version'   => 'latest',
-    'region'    =>  'us-east-2'
+    'version'       => 'latest',
+    'region'        =>  'us-east-2',
+    'credentials'   =>  $credentials
 ];
 $sdk = new Sdk($config);
-$client = $sdk->createS3();
+$client = $sdk->createCloudWatchLogs();
 echo '<hr><pre>';
 echo 'File :: ' . __FILE__ . ' (Line ' . __LINE__ . ')' . PHP_EOL;
-var_dump($client->listBuckets());
+var_dump($client->createLogGroup([
+    'logGroupName' => 'test_1', // REQUIRED
+    'tags' => ['test' => '1'],
+]));
 echo '</pre><hr>';
 exit();
